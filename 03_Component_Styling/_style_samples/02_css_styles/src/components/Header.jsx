60fec7b3d803fc5b647cb1ed0772f10fee901ab3
rdndev <rdninc@mail.ru>
2025-01-17 15:53:09 +0200

    feat(03_Component_Styling): enhance component styling  and add sample project structure.

---
+++ 03_Component_Styling/_style_samples/02_css_styles/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import "../styles/components/Header.css";
+
+function Header() {
+  return (
+    <header className="header">
+      <h1 className="header__title">blog-app</h1>
+    </header>
+  );
+}
+
+export default Header;
