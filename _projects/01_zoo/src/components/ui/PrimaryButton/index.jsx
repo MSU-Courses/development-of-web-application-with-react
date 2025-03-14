3cabc7774b2520bb2c96e14c1958d1f5280c4f1d
/rdndev <rdninc@mail.ru>
2025-03-14 15:35:01 +0200

    feat: add hero

--- _projects/01_zoo/src/components/ui/PrimaryButton/index.jsx
+++ _projects/01_zoo/src/components/ui/PrimaryButton/index.jsx
@@ -2,8 +2,6 @@ import styles from "./PrimaryButton.module.scss";
 
 function PrimaryButton({
   text,
-  padding = { x: "20px", y: "10px" },
-  bgColor = "#006400",
   className = "",
 }) {
   return (
